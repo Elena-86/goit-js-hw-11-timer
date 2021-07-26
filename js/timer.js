7053ee7bef47ccddb0758304a241a6f79aa02c1c
Tiutiunnyk Elena <elena.tiutiunnyk@gmail.com>
2021-07-26 18:43:12 +0300

    home work 11 with cleared interval

--- js/timer.js
+++ js/timer.js
@@ -1,42 +1,51 @@
 class CountdownTimer {
-  constructor({selector,targetDate,onTick}) {
+  constructor({ selector, targetDate, onTick }) {
     this.targetDate = targetDate;
     this.onTick = onTick;
     const timerRef = document.querySelector(selector);
     this.refs = {
-  days: timerRef.querySelector('span[data-value="days"]'),
-  hours: timerRef.querySelector('span[data-value="hours"]'),
-  mins: timerRef.querySelector('span[data-value="mins"]'),
-  secs: timerRef.querySelector('span[data-value="secs"]')
-}
+      days: timerRef.querySelector('span[data-value="days"]'),
+      hours: timerRef.querySelector('span[data-value="hours"]'),
+      mins: timerRef.querySelector('span[data-value="mins"]'),
+      secs: timerRef.querySelector('span[data-value="secs"]')
+    }
   }
 
   start() {
-      setInterval(() => {
+    let intervalId = setInterval(() => {
+
+
       const currentTime = Date.now();
-      const deltaTime = this.targetDate-currentTime;
+      const deltaTime = this.targetDate - currentTime;
       const time = this.createTimeConponents(deltaTime);
-      
-        this.onTick(time, this.refs)
-    
+      if (time.days < 0 || time.hours < 0 || time.minutes < 0 || time.seconds < 0) {
+        clearInterval(intervalId);
+      } else {
+        this.onTick(time, this.refs);
+      }
+
+
     }, 1000);
-  }
-  
-  createTimeConponents(time) {  
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));  
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));  
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
 
-  return { days, hours, mins, secs };
 
   }
-  
+
+  createTimeConponents(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+    return { days, hours, mins, secs };
+
+  }
+
+
 };
 
 const countdownTimer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2021'),
+  targetDate: new Date('Aug 17, 2021'),
   onTick: updateTimerInterfase
 });
 
